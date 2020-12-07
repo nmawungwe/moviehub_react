eb1116b3ff16f3987e7243ddbef2d5894034c23f
Nyasha Vision <nmawungwe@gmail.com>
2020-12-07 16:16:43 +0200

    first commit for moviehub app using react

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {ActivityIndicator, FlatList, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default class App extends Component {
+
+  state = {
+            title: '',
+  }
+
+  titleHandler = title => {
+    this.setState({title})
+  }
+
+  searchMovieFromApi = () => {
+    const title = this.state.title
+
+    fetch(`http://www.omdbapi.com/?s=${title}&apikey=62927f24`).then(response => response.json()).then((json)=>{
+      console.log(json)
+    }).catch((error) => console.error(error))
+
+    
+    
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Movie Hub</Text>
+        <TextInput
+          onChangeText={this.titleHandler}
+          placeholder="Enter movie title"
+          value={this.state.title}
+        />
+        <Button 
+          title="Search"  
+          onPress={this.searchMovieFromApi}        
+        />
+      </View>
+        )
+      } 
+    }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
